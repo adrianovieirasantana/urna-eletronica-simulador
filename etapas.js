71ba06a274ce76e179e4a69189ab6ff4613355c5
Adriano Vieira da Silva Santana <74624979+adriano-santana@users.noreply.github.com>
2022-09-04 23:08:05 -0300

    commit init
    Commit arquivos simulador Urna Eletrônica.

---
+++ etapas.js
@@ -0,0 +1,102 @@
+let etapas = [
+    //Dados para votar - Deputado Federal
+    {
+        titulo: 'DEPUTADO FEDERAL',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: '4411',
+                nome: 'Defensor dos Animais',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Deputado(a) Federal'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Deputado(a) Federal'}
+                ]
+            },
+        ]
+    },
+      //Dados para votar - Senador
+    {
+        titulo: 'SENADOR',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '222',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Senador(a)'}
+                ]
+            },
+            {
+                numero: '221',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Senador(a)'}
+                ]
+            },
+        ]
+    },
+    //Dados para votar - Governador
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '55',
+                nome: 'Ratinho',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Governador'},
+                    {url:'99_2.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Governador'},
+                    {url:'84_2.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+        ]
+    },
+        //Dados para votar - Presidente
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'Mito',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'99_2.jpg', legenda: 'Presidente'},
+                    {url:'99.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Presidente'},
+                    {url:'84_2.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+        ]
+    }
+];
